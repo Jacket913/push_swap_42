88fa8d18b805c8c3d34ab46d2a3b60548cf9569f
Jacket913 <>
2024-04-30 18:57:17 +0200

    Functions done

--- src/rotate.c
+++ src/rotate.c
@@ -3,26 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
+/*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:26:38 by gmoulin           #+#    #+#             */
-/*   Updated: 2024/04/26 10:43:57 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2024/04/30 18:35:40 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void	ra(stack_t **a)
-// {
+void	ra(t_stack **a)
+{
+	t_stack *nlast;
+	t_stack *last;
 
-// }
+	if(!*a)
+		return ;
+	nlast = *a;
+	last = *a;
+	while (last->next)
+		last = last->next;
+	last->next = nlast;
+	nlast->prev = last;
+	*a = nlast->next;
+	nlast->next = NULL;
+	(*a)->prev = NULL;
+	ft_putendl_fd("ra", 1);
+}
 
-// void	rb(stack_t **b)
-// {
+void	rb(t_stack **b)
+{
+	t_stack *nlast;
+	t_stack *last;
 
-// }
+	if(!*b)
+		return ;
+	nlast = *b;
+	last = *b;
+	while (last->next)
+		last = last->next;
+	last->next = nlast;
+	nlast->prev = last;
+	*b = nlast->next;
+	nlast->next = NULL;
+	(*b)->prev = NULL;
+	ft_putendl_fd("rb", 1);
+}
 
-// void	rr(stack_t **a, stack_t **b)
-// {
-
-// }
+void	rr(stack_t **a, stack_t **b)
+{
+	ra(a);
+	rb(b);
+	ft_putendl_fd("rr", 1);
+}
--- src/rrotate.c
+++ src/rrotate.c
@@ -3,27 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   rrotate.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
+/*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:26:46 by gmoulin           #+#    #+#             */
-/*   Updated: 2024/04/26 10:43:55 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2024/04/30 18:54:05 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
-// void	rra(t_stack **a)
-// {
+void	rra(t_stack **a)
+{
+	t_stack *nfirst;
+	t_stack *first;
 
-// }
+	if(!*a)
+		return ;
+	if (*a && (*a)->next)
+	{
+		nfirst = *a;
+		first = *a;
+		while (nfirst->next)
+			nfirst = nfirst->next;
+		first->prev = nfirst;
+		nfirst->next = first;
+		*a = nfirst;
+		nfirst->prev->next = NULL; //set new last ->next to NULL
+		nfirst->prev = NULL;
+		ft_putendl_fd("rra", 1);
+	}
+}
 
-// void	rrb(t_stack **b)
-// {
+void	rrb(t_stack **b)
+{
+	t_stack *nfirst;
+	t_stack *first;
 
-// }
+	if(!*b)
+		return ;
+	if (*b && (*b)->next)
+	{
+		nfirst = *b;
+		first = *b;
+		while (nfirst->next)
+			nfirst = nfirst->next;
+		first->prev = nfirst;
+		nfirst->next = first;
+		*b = nfirst;
+		nfirst->prev->next = NULL; //set new last ->next to NULL
+		nfirst->prev = NULL;
+		ft_putendl_fd("rrb", 1);
+	}
+}
 
-// void	rrr(t_stack **a, t_stack **b)
-// {
-
-// }
+void	rrr(t_stack **a, t_stack **b)
+{
+	rra(a);
+	rrb(b);
+	ft_putendl_fd("rrr", 1);
+}
